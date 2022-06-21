cc5428a541ec36c02457e18a4b0e7803aa3f0d31
Ben Clark <benclarkphoto@mac.com>
2022-06-20 22:55:15 -0700

    add get gradient function for gradient background changes

--- app.js
+++ app.js
@@ -19,16 +19,17 @@ const leaveAnimation = (current, done) => {
     );
 };
 
-const enterAnimation = (current, done) => {
+const enterAnimation = (current, done, gradient) => {
     const product = current.querySelector('.image-container');
     const text = current.querySelector(".showcase-text");
     const circles = current.querySelectorAll(".circle");
     const arrow = current.querySelector(".showcase-arrow");
     return (
-        tlLeave.fromTo(arrow, {opacity: 0, y: 50}, {opacity: 1, y: 0}),
-        tlLeave.fromTo(product, {y: -100, opacity: 0}, {y: 0, opacity: 1, onComplete: done}, "<"),
-        tlLeave.fromTo(text, {y: 100, opacity: 0}, {opacity: 1, y: 0}, "<"),
-        tlLeave.fromTo(circles, {y: -200, opacity: 0}, {y: -200, opacity: 1, stagger: 0.15, ease: "back.out(1.7)", duration: 1}, "<")
+        tlEnter.fromTo(arrow, {opacity: 0, y: 50}, {opacity: 1, y: 0}),
+        tlEnter.to("body", {background: gradient}, "<"),
+        tlEnter.fromTo(product, {y: -100, opacity: 0}, {y: 0, opacity: 1, onComplete: done}, "<"),
+        tlEnter.fromTo(text, {y: 100, opacity: 0}, {opacity: 1, y: 0}, "<"),
+        tlEnter.fromTo(circles, {y: -200, opacity: 0}, {y: -200, opacity: 1, stagger: 0.15, ease: "back.out(1.7)", duration: 1}, "<")
     );
 };
 
@@ -39,6 +40,13 @@ barba.init({
         // showcase transitions
         {
             name: "default",
+            once(data) {
+                const done = this.async();
+                let next = data.next.container;
+                let gradient = getGradient(data.next.namespace);
+                gsap.set("body", {background: gradient});
+                enterAnimation(next, done, gradient);
+            },
             leave(data) {
                 const done = this.async();
                 let current = data.current.container;
@@ -47,9 +55,22 @@ barba.init({
             enter(data) {
                 const done = this.async();
                 let next = data.next.container;
-                enterAnimation(next, done);
+                let gradient = getGradient(data.next.namespace);
+                enterAnimation(next, done, gradient);
                 // gsap.fromTo(next, {opacity: 0}, {opacity: 1, duration: 1, onComplete: done});
             }
         },
     ],
-})
+});
+
+// Changing gradient on showcase
+function getGradient(name) {
+    switch(name) {
+        case "handbag":
+            return "linear-gradient(260deg, #b75d62, #754d4f)";
+        case "boot":
+            return "linear-gradient(260deg, #5d8cb7, #4c4f17)";
+        case "hat":
+            return "linear-gradient(260deg, #b27a5c, #7f5459)";
+    }
+}
